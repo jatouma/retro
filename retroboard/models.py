cbc45cc24283c858b2b06d97a8dc5ec7d3ad9482
Jean Marc Touma <jatouma@Jeans-MacBook-Pro.local>
2018-05-07 11:19:25 -0400

    Revert "Revert "Implement Retro Board""
    This reverts commit de8fb33aadad66a145878d3c09631169d2063999.

---
+++ retroboard/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+# Create your models here.
+
+class Board(models.Model):
+	sprint = models.CharField(max_length=64, unique=True)
+
+	def __str__(self):
+		return f"Sprint: {self.sprint}"
+
+#Sticky is short for Sticky Note, Physical sticky notes are used on physical retro boards.
+class Sticky(models.Model):
+	parent_board = models.ForeignKey(Board, on_delete=models.PROTECT, default=1)
+	user = models.ForeignKey(get_user_model(), null=True, blank=True, on_delete=models.SET_NULL)
+	note = models.CharField(max_length=512)
+	created = models.DateTimeField(auto_now_add=True)
+
+	def get_submitter(self):
+		if self.user is not None:
+			return self.user
+		return "Anon"
+
+	class Meta:
+		abstract = True
+
+class ActionItem(Sticky):
+	is_completed = models.BooleanField(default=False)
+
+	def __str__(self):
+		return f"Action Item: {self.note}"
+
+class Positive(Sticky):
+	pass
+
+	def __str__(self):
+		return f"Positive: {self.note}"
+
+class Delta(Sticky):
+	pass
+
+	def __str__(self):
+		return f"Delta: {self.note}"
+
+
+
